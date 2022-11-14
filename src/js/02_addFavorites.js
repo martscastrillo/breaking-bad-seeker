3096f79b6fb21fd6e82aad3d3078a211ae8dfaa2
Marta Castrillo <mart.castrillo@gmail.com>
2022-11-14 21:17:46 +0100

    improve remove favs

--- src/js/02_addFavorites.js
+++ src/js/02_addFavorites.js
@@ -1,6 +1,6 @@
 
 function renderfavoriteCh() {
-  
+
     let html = '';
     for (const eachCharacter of favoritesCharacters) {
         html += renderFAVCharacter(eachCharacter);
@@ -8,33 +8,30 @@ function renderfavoriteCh() {
     favoriteList.innerHTML = html;
     const xfavorite = document.querySelectorAll('.js_x_favorite');
     for (const onex of xfavorite) {
-      onex.addEventListener('click', handleClickCancellFav);
+        onex.addEventListener('click', handleClickCancellFav);
     }
 }
 
-function handleClickCancellFav(event){
+function handleClickCancellFav(event) {
     event.preventDefault();
     const characterInFavsIndex = favoritesCharacters.findIndex((charac) => charac.char_id === parseInt(event.currentTarget.id));
-    console.log(characterInFavsIndex);
-    if (characterInFavsIndex === -1) {
+
+    favoritesCharacters.splice(characterInFavsIndex, 1);
+    localStorage.removeItem('favoriteCharacter');
+    renderfavoriteCh();
+    localStorage.setItem('favoriteCharacter', JSON.stringify(favoritesCharacters));
+    //////
+    console.log(parseInt(event.currentTarget.id));
+    const currentTargetId =allCharactersCard[parseInt(event.currentTarget.id)];
+    console.log(currentTargetId);
+  
+    //busqueda para saber la posicion en favoritos si la tarjeta está en favoritos
+
+    /*  if (event.currentTarget.id !== favsInAll) {
+         event.currentTarget.classList.remove('selected');
      
-    }
  
-    else {
-        favoritesCharacters.splice(characterInFavsIndex, 1); 
-        localStorage.removeItem('favoriteCharacter');
-        renderfavoriteCh();
-        localStorage.setItem('favoriteCharacter', JSON.stringify(favoritesCharacters));
  
-
- /*    if (onex.id === ) {
-   
-    }
-
-    else {
-      
-
-    } */
-    }
-
+     }
+  */
 }
